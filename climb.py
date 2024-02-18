370364b5de66e935134222164e66dd53cd2fb612
liuutin9 <136707519+liuutin9@users.noreply.github.com>
2024-02-18 17:19:42 +0800

    update

--- climb.py
+++ climb.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from openpyxl import Workbook, load_workbook
 
 def get_stock_price(symbol):
     url = f"https://tw.finance.yahoo.com/quote/{symbol}"
@@ -33,7 +34,8 @@ def get_exchange_rate_USD():
     return float(data[0]['SubInfo'][0]['DataValue2'])
 
 
-exchange_rate_USD = get_exchange_rate_USD()
+
+wb = load_workbook("帳目表.xlsx")
 
 # 股票代碼
 stock_symbols = ["006208.TW", "00692.TW", "00878.TW", "2890.TW", "BND", "VEA", "VT", "VTI"]
@@ -41,19 +43,24 @@ stock_symbols = ["006208.TW", "00692.TW", "00878.TW", "2890.TW", "BND", "VEA", "
 # 獲取股價
 stock_prices = {}
 
-# print(stock_symbols[0][0:-3])
-
+# modify excel file
 for symbol in stock_symbols:
-    wb[symbol]
-    stock_prices[symbol] = get_stock_price(symbol)
+    if (symbol[len(symbol)-3] == '.'):
+        wb[symbol]["C3"].value = get_stock_price(symbol)
+    else:
+        wb[symbol]["H3"].value = get_stock_price(symbol)
+        
+wb["投資"]["G2"].value = get_exchange_rate_USD()
     
 wb.save("帳目表.xlsx")
     
 # 寫入 txt 檔
-out = open("stock_prices.txt", mode = "w")
-for i in range(len(stock_symbols)):
-    out.write(f"{stock_symbols[i]},{stock_prices[i]}\n")
-out.write(f"USD,{exchange_rate_USD}\n")
-out.close()
+# out = open("stock_prices.txt", mode = "w")
+# for i in range(len(stock_symbols)):
+#     out.write(f"{stock_symbols[i]},{stock_prices[i]}\n")
+# out.write(f"USD,{exchange_rate_USD}\n")
+# out.close()
 
-print("股價已成功寫入 stock_prices.txt 檔案中.")
+# print("股價已成功寫入 stock_prices.txt 檔案中.")
+
+print("Success!")
