35bdbf586ebd0392f2ec10c4c3410b14cb4caadc
liuutin9 <136707519+liuutin9@users.noreply.github.com>
2024-09-09 14:09:11 +0800

    update

--- climb.py
+++ climb.py
@@ -1,9 +1,7 @@
-import gc
 import requests
 from bs4 import BeautifulSoup
 import json
-from openpyxl import Workbook, load_workbook
-import schedule
+from openpyxl import load_workbook
 import datetime
 
 def get_stock_price(symbol):
@@ -36,27 +34,13 @@ def get_exchange_rate_USD():
     
     return float(data[0]['SubInfo'][0]['DataValue2'])
 
-# wb_getValue = load_workbook("帳目表.xlsx", data_only = True, read_only = True)
-# lastData = wb_getValue["投資"]["F10"].value
-# print(lastData)
-# if (type(lastData) != 'NoneType'):
-#     lastValue = float(wb_getValue["投資"]["F10"].value)
-# wb_getValue.close()
-# del wb_getValue
-# gc.collect()
-
 wb = load_workbook("帳目表.xlsx")
 
-# if (type(lastData) != 'NoneType'):
-#     wb["投資"]["A16"].value = lastValue
-
 # 股票代碼
-stock_symbols = ["006208.TW", "00692.TW", "00878.TW", "2890.TW", "BND", "VEA", "VT", "VTI"]
+stock_symbols = ["006208.TW", "00692.TW", "00878.TW", "2890.TW", "BND", "VT"]
 
 # 獲取股價
 stock_prices = {}
-
-# wb["投資"]["A16"].value = float(wb["投資"]["F10"].value)
 
 # modify excel file
 for symbol in stock_symbols:
@@ -66,20 +50,9 @@ for symbol in stock_symbols:
         wb[symbol]["H3"].value = get_stock_price(symbol)
         
 wb["投資"]["G2"].value = get_exchange_rate_USD()
-
-# wb["投資"]["B16"].value = float(wb["投資"]["F10"].value)
     
 wb.save("帳目表.xlsx")
 wb.close()
-
-# 寫入 txt 檔
-# out = open("stock_prices.txt", mode = "w")
-# for i in range(len(stock_symbols)):
-#     out.write(f"{stock_symbols[i]},{stock_prices[i]}\n")
-# out.write(f"USD,{exchange_rate_USD}\n")
-# out.close()
-
-# print("股價已成功寫入 stock_prices.txt 檔案中.")
 
 now = datetime.datetime.now().replace(microsecond = 0)
 
