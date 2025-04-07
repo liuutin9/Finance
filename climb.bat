4a11566f5837ae0a83dca4d1ac0a7bbe2d4ec569
liuutin9 <136707519+liuutin9@users.noreply.github.com>
2025-04-07 12:01:23 +0800

    daily update

--- climb.bat
+++ climb.bat
@@ -2,8 +2,8 @@
 chcp 65001
 cd "C:\Users\Utin Liu\OneDrive\桌面\Finance\"
 git pull --rebase
-py climb.py
-py stock_repo.py
+python climb.py
+python stock_repo.py
 git checkout main
 git add .
 git commit -m "daily update"
