10cf8b842d350c864895ef0d6a325c6394532c02
liuutin9 <136707519+liuutin9@users.noreply.github.com>
2026-07-12 16:40:34 +0800

    Update climb.bat

--- climb.bat
+++ climb.bat
@@ -2,10 +2,5 @@
 chcp 65001
 cd "C:\Users\Utin Liu\Desktop\Finance\"
 git pull --rebase
-python main.py
-git checkout main
-git add .
-git commit -m "daily update"
-git push -u origin main
 #pause
 exit
